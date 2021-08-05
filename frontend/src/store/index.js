220238bced56afd3bfdf3d340096930389b64097
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-08-05 20:06:44 +0300

    Add language setting in the store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,8 +4,9 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
+    state: {
+        lang: 'TR',
+    },
     actions: {},
     modules: {},
 });
