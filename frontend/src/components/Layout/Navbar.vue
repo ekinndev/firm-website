433b2b4e714ce8e8e0a82e3a8043570cce9019a6
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-08-07 13:05:41 +0300

    Add user info component

--- frontend/src/components/Layout/Navbar.vue
+++ frontend/src/components/Layout/Navbar.vue
@@ -1,9 +1,15 @@
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 import LanguageSelector from '../LocaleSelector.vue';
+import UserInfo from '../UserInfo.vue';
+
 export default {
     components: {
         LanguageSelector,
+        UserInfo,
+    },
+    computed: {
+        ...mapState(['user']),
     },
     methods: {
         ...mapMutations(['toggleModal']),
@@ -35,7 +41,10 @@ export default {
                 <li class="nav-item">
                     <router-link to="/contact" class="nav-item">{{ $t('message.Contact') }}</router-link>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" v-if="user">
+                    <user-info />
+                </li>
+                <li class="nav-item" v-if="!user">
                     <button @click="toggleModal">Login</button>
                 </li>
             </ul>
---
+++ frontend/src/components/UserInfo.vue
@@ -0,0 +1,50 @@
+<template>
+    <button class="user-info-btn" @click="toggleInfo">
+        User Info
+        <div class="user-info" v-if="showInfo">
+            <div class="user-info-email">{{ user.email }}</div>
+            <button @click="logout">Logout</button>
+        </div>
+    </button>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+    data: () => ({
+        showInfo: false,
+    }),
+    computed: {
+        ...mapState(['user']),
+    },
+    methods: {
+        ...mapActions(['logout']),
+
+        toggleInfo() {
+            this.showInfo = !this.showInfo;
+        },
+    },
+};
+</script>
+
+<style>
+.user-info-btn {
+    position: relative;
+}
+
+.user-info {
+    background: #fff;
+    border: 1px solid #333;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px;
+    gap: 20px;
+    align-items: center;
+    height: 100px;
+    position: absolute;
+    top: 1rem;
+    left: 1;
+}
+</style>
